9d7db2f469854b668a8d7a2d67948f19d0477af8
Fufu <fufuok@qq.com>
2022-01-17 09:17:15 +0800

    chore: add example certificate file path

--- script/xydatarouter.sh
+++ script/xydatarouter.sh
@@ -2,3 +2,5 @@
 # source /etc/profile.d/xydatarouter.sh
 export DR_BASE_SECRET_KEY=CXPJXUoN2uAo9p5DQhnwgt
 export DR_XY_MONITOR_API_KEY=7T85ceT9Bjjt4z6gXfbFWJd4FrL5w8PNydMvJJAi3LYE
+export DR_WEB_CERT_FILE=/etc/nginx/ssl/server.crt
+export DR_WEB_KEY_FILE=/etc/nginx/ssl/server.key
