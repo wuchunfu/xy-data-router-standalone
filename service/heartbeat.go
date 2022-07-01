707e691a810b5560437a27e7e6d7571d0704b020
Fufu <fufuok@qq.com>
2022-07-01 17:48:28 +0800

    perf: optimize heartbeat log

--- service/heartbeat.go
+++ service/heartbeat.go
@@ -1,10 +1,9 @@
 package service
 
 import (
-	"fmt"
 	"time"
 
-	"github.com/fufuok/utils"
+	"github.com/fufuok/utils/jsongen"
 
 	"github.com/fufuok/xy-data-router/common"
 	"github.com/fufuok/xy-data-router/conf"
@@ -15,10 +14,14 @@ import (
 func initHeartbeat() {
 	ticker := common.TWm.NewTicker(time.Minute)
 	defer ticker.Stop()
-
 	for range ticker.C {
-		data := utils.S2B(fmt.Sprintf(`{"type":"%s","version":"%s","internal_ipv4":"%s","external_ipv4":"%s"}`,
-			conf.APPName, conf.Version, InternalIPv4, ExternalIPv4))
+		js := jsongen.NewMap()
+		js.PutString("type", conf.APPName)
+		js.PutString("version", conf.Version)
+		js.PutString("internal_ipv4", InternalIPv4)
+		js.PutString("external_ipv4", ExternalIPv4)
+		js.PutString("time", time.Now().Format(time.RFC3339))
+		data := js.Serialize(nil)
 		item := schema.New(conf.Config.SYSConf.HeartbeatIndex, ExternalIPv4, data)
 		PushDataToChanx(item)
 	}
