412bb838f8ddeed6d3b6aa640363ee86063f96f3
Fufu <fufuok@qq.com>
2022-07-15 08:53:02 +0800

    feat: add MustJSONString function

--- internal/json/json.go
+++ internal/json/json.go
@@ -1,5 +1,9 @@
 package json
 
+import (
+	"unsafe"
+)
+
 // MustJSONIndent 转 json 返回 []byte
 func MustJSONIndent(v interface{}) []byte {
 	js, _ := MarshalIndent(v, "", "  ")
@@ -10,4 +14,10 @@ func MustJSONIndent(v interface{}) []byte {
 func MustJSON(v interface{}) []byte {
 	js, _ := Marshal(v)
 	return js
+}
+
+// MustJSONString 转 json 返回 string
+func MustJSONString(v interface{}) string {
+	js := MustJSON(v)
+	return *(*string)(unsafe.Pointer(&js))
 }
